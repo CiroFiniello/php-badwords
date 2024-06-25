cddf7620f02d90629e0ece8028d10fd332222517
CodeKiro <kirofiniello@outlook.it>
2024-06-25 21:39:21 +0200

    ho utilizzato isset per controllare se la variabile inserita esiste e la funzione str_replace per sostituire le e con *

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$name = $_GET["ciao"];
-$cognome = "finiello";
+// Check if 'censuro' key exists in $_GET before using it
+$censuro = isset($_GET["censuro"]) ? $_GET["censuro"] : '';
+
+// Replace 'e' with '*' in the censuro variable
+$censored = str_replace("e", "*", $censuro);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +22,22 @@ $cognome = "finiello";
     echo strlen($test);
     ?>
     </p>
-
-    </form>
+<h1>Parte due dell'esercizio:</h1>
+<form action="./index.php" method="GET">
+    <input type="text" name="censuro" id="i-censuro">
+    <button type="submit"> CENSURA! </button>
+</form>
+    <h1>
+        la parola che verrà modificata è :
+        <?php
+        echo $censored;
+        ?>
+    </h1>
+        
+    <pre>
+            <?php 
+            // var_dump($_GET);
+            ?>
+    </pre>
 </body>
 </html>
