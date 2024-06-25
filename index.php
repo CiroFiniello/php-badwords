1b3bf51323e79e82245653954b34b2734d34cb93
CodeKiro <kirofiniello@outlook.it>
2024-06-25 19:42:23 +0200

    Ho creato uno scaffolding, e ho utilizzato strlen come funzione per determinare la lunghezza della variabile "test", e poi ho usato echo per stamparla a schermo ( sia la variabile che la lunghezza)

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_GET["ciao"];
+$cognome = "finiello";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <p>
+    <?php
+    $test = "bestie";
+    echo "zao! ";
+    echo $test;
+    echo strlen($test);
+    ?>
+    </p>
+
+    </form>
+</body>
+</html>
