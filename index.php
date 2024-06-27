99d166dfc08bf9bf4d9856ab7e1d90bd50a0cfd3
CodeKiro <kirofiniello@outlook.it>
2024-06-27 23:09:54 +0200

    ho creato una variabile symbol, e una variabile badword con un controllo sulla parola tramite ternario, e poi ho usato str_ireplace per censurare la parola scritta in un form atto allo scopo

--- index.php
+++ index.php
@@ -1,43 +1,36 @@
 <?php
-// Check if 'censuro' key exists in $_GET before using it
-// $censuro = isset($_GET["censuro"]) ? $_GET["censuro"] : '';
-    $censuro = $_GET["censuro"];
-// Replace 'e' with '*' in the censuro variable
-$censored = str_replace("e", "*", $censuro);
+$paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel magnam asperiores molestias consectetur. Soluta dicta, voluptas possimus officia ex omnis similique quidem ipsa quia corporis nostrum, earum vero, laborum quibusdam.
+Recusandae est aperiam quam, nihil quas inventore beatae quisquam a tempore, sed itaque iure esse hic praesentium culpa pariatur, provident quasi aliquid illum nemo. Nam vitae tempora facilis fugit quod.
+Numquam consequuntur laudantium quisquam dicta blanditiis nostrum quis accusantium nam laboriosam. Ipsam animi aperiam inventore quidem sunt eligendi repellendus atque nobis dolorum repellat sequi excepturi aliquid itaque, ex beatae distinctio?';
+
+$symbol = "*****";
+$badword = isset($_GET['badword']) ? $_GET['badword'] : '';
+$_censoredParagraph = str_ireplace($badword, $symbol, $paragraph);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-badwords</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
-    <p>
-    <?php
-    $test = "bestie";
-    echo "zao! ";
-    echo $test;
-    echo strlen($test);
-    ?>
-    </p>
-<h1>Parte due dell'esercizio:</h1>
-<form action="./index.php" method="GET">
-    <input type="text" name="censuro" id="i-censuro">
-    <button type="submit"> CENSURA! </button>
-</form>
-    <h1>
-        la parola che verrà modificata è :
-        <?php
-        echo $censored;
-        ?>
-    </h1>
-        
-    <pre>
-            <?php 
-            // var_dump($_GET);
-            ?>
-    </pre>
+    <section class="plong">
+        <h1>Paragrafo iniziale:</h1>
+        <p><?php echo $paragraph; ?></p>
+        <h1>La lunghezza del paragrafo è: <?php echo strlen($paragraph); ?> caratteri</h1>
+    </section>
+    <section class="pmod">
+        <h1>Paragrafo modificato:</h1>
+        <p><?php echo $_censoredParagraph; ?></p>
+        <h1>La lunghezza del paragrafo è: <?php echo strlen($_censoredParagraph); ?> caratteri</h1>
+    </section>
+    <form action="" method="GET">
+        <label for="badword">Parola da censurare:</label>
+        <input type="text" name="badword" id="badword">
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
